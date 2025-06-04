85e3e607da924fe6d5fe79048f653b9a75960032
zubair-techcollegues <zubair.liaqat2701@gmail.com>
2025-06-04 14:20:39 +0500

    change

--- CleanArchitectureCQRSPatteren.Application/obj/Debug/net8.0/CleanArchitectureCQRSPatteren.Application.AssemblyInfo.cs
+++ CleanArchitectureCQRSPatteren.Application/obj/Debug/net8.0/CleanArchitectureCQRSPatteren.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitectureCQRSPatteren.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b43a4f3c8b5371bee386fde66cb33121e91126a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdd866eb927479cbed905a3add838fb0452f2fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchitectureCQRSPatteren.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitectureCQRSPatteren.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
